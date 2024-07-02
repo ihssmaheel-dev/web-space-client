b075af5d6059712bf8800d6a350a1cbc5a7ee5ff
ismail-hepl <ismail.s@hepl.com>
2024-07-02 16:27:45 +0530

    Fix: Fix the 'op.current' is possibly 'null'.ts(18047) issue in the WebsiteCard

--- src/components/WebsiteCard.tsx
+++ src/components/WebsiteCard.tsx
@@ -7,12 +7,12 @@ interface WebsiteCardProps {
 }
 
 const WebsiteCard: React.FC<WebsiteCardProps> = ({ title, description }) => {
-    const op = useRef(null);
+    const op = useRef<OverlayPanel>(null);
 
     return (
         <div className="custom-card h-10rem p-2 cursor-pointer border-round-md shadow-1 bg-white relative">
             <div className="text-right">
-                <i className="pi pi-info-circle text-primary" onClick={(e) => op.current.toggle(e)}></i>
+                <i className="pi pi-info-circle text-primary" onClick={(e) => op.current && op.current.toggle(e)}></i>
             </div>
             <p className="m-0 p-0 flex justify-content-center align-items-center absolute top-50 left-50 card-content text-base font-bold uppercase" style={{transform: 'translate(-50%, -50%)'}}>
                 {title}
