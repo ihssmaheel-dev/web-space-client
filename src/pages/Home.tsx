636bef8cac193b79744d729d06d9b8929e7dfc84
ismail-hepl <ismail.s@hepl.com>
2024-07-02 22:50:03 +0530

    Feat: remove all the categories except only one and rename it to category

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,83 +25,11 @@ const Home: React.FC = () => {
 
     const categories: CategoryI[] = [
         {
-            name: 'Technology',
-            icon: 'pi pi-fw pi-desktop',
+            name: 'Category',
+            icon: 'pi pi-fw pi-stop',
             websites: [
                 { name: "TechCrunch", description: "Tech news", link: "https://techcrunch.com" },
                 { name: "The Verge", description: "Tech reviews", link: "https://www.theverge.com" }
-            ]
-        },
-        {
-            name: 'Sports',
-            icon: 'pi pi-fw pi-football',
-            websites: [
-                { name: "ESPN", description: "Sports news", link: "https://www.espn.com" },
-                { name: "NFL.com", description: "NFL news", link: "https://www.nfl.com" }
-            ]
-        },
-        {
-            name: 'Cooking',
-            icon: 'pi pi-fw pi-utensils',
-            websites: [
-                { name: "AllRecipes", description: "Recipes", link: "https://www.allrecipes.com" },
-                { name: "Serious Eats", description: "Food culture", link: "https://www.seriouseats.com" }
-            ]
-        },
-        {
-            name: 'Finance',
-            icon: 'pi pi-fw pi-money-bill',
-            websites: [
-                { name: "Bloomberg", description: "Financial news", link: "https://www.bloomberg.com" },
-                { name: "Yahoo Finance", description: "Stock market info", link: "https://finance.yahoo.com" }
-            ]
-        },
-        {
-            name: 'Travel',
-            icon: 'pi pi-fw pi-globe',
-            websites: [
-                { name: "Lonely Planet", description: "Travel guides", link: "https://www.lonelyplanet.com" },
-                { name: "TripAdvisor", description: "Travel reviews", link: "https://www.tripadvisor.com" }
-            ]
-        },
-        {
-            name: 'Health',
-            icon: 'pi pi-fw pi-heart',
-            websites: [
-                { name: "WebMD", description: "Health advice", link: "https://www.webmd.com" },
-                { name: "Healthline", description: "Medical information", link: "https://www.healthline.com" }
-            ]
-        },
-        {
-            name: 'Entertainment',
-            icon: 'pi pi-fw pi-video',
-            websites: [
-                { name: "IMDb", description: "Movie database", link: "https://www.imdb.com" },
-                { name: "Variety", description: "Entertainment news", link: "https://variety.com" }
-            ]
-        },
-        {
-            name: 'News',
-            icon: 'pi pi-fw pi-newspaper',
-            websites: [
-                { name: "BBC News", description: "World news", link: "https://www.bbc.com/news" },
-                { name: "CNN", description: "Breaking news", link: "https://www.cnn.com" }
-            ]
-        },
-        {
-            name: 'Education',
-            icon: 'pi pi-fw pi-graduation-cap',
-            websites: [
-                { name: "Khan Academy", description: "Online education", link: "https://www.khanacademy.org" },
-                { name: "Coursera", description: "Online courses", link: "https://www.coursera.org" }
-            ]
-        },
-        {
-            name: 'Shopping',
-            icon: 'pi pi-fw pi-shopping-cart',
-            websites: [
-                { name: "Amazon", description: "Online shopping", link: "https://www.amazon.com" },
-                { name: "Etsy", description: "Handmade goods", link: "https://www.etsy.com" }
             ]
         }
     ];
