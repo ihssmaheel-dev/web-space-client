d9ac4d57c849a5d3b374535309162cb5e2175b85
ismail-hepl <ismail.s@hepl.com>
2024-07-02 16:24:28 +0530

    Feat: Rename the CustomCard into WebsiteCard, Add OverlayPanel for the description

---
+++ src/components/WebsiteCard.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import { OverlayPanel } from 'primereact/overlaypanel';
+
+interface WebsiteCardProps {
+    title: string
+    description: string;
+}
+
+const WebsiteCard: React.FC<WebsiteCardProps> = ({ title, description }) => {
+    const op = useRef(null);
+
+    return (
+        <div className="custom-card h-10rem p-2 cursor-pointer border-round-md shadow-1 bg-white relative">
+            <div className="text-right">
+                <i className="pi pi-info-circle text-primary" onClick={(e) => op.current.toggle(e)}></i>
+            </div>
+            <p className="m-0 p-0 flex justify-content-center align-items-center absolute top-50 left-50 card-content text-base font-bold uppercase" style={{transform: 'translate(-50%, -50%)'}}>
+                {title}
+            </p>
+            <OverlayPanel ref={op} className='w-4'>
+                <p>{description}</p>
+            </OverlayPanel>
+        </div>
+    )
+}
+
+export default WebsiteCard
