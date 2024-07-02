fc1ac8ad08090620cc08c51d00df2f04d27eceaa
ismail-hepl <ismail.s@hepl.com>
2024-07-02 16:58:13 +0530

    Refactor: Optimize and Change the onClick with onMouseOver and OnMouseOut

--- src/components/WebsiteCard.tsx
+++ src/components/WebsiteCard.tsx
@@ -2,26 +2,35 @@ import React, { useRef } from 'react';
 import { OverlayPanel } from 'primereact/overlaypanel';
 
 interface WebsiteCardProps {
-    title: string
+    title: string;
     description: string;
 }
 
 const WebsiteCard: React.FC<WebsiteCardProps> = ({ title, description }) => {
     const op = useRef<OverlayPanel>(null);
 
+    const handleMouseOver = (e: React.MouseEvent) => op.current?.show(e, e.currentTarget);
+    const handleMouseOut = () => op.current?.hide();
+
     return (
         <div className="custom-card h-10rem p-2 cursor-pointer border-round-md shadow-1 bg-white relative">
             <div className="text-right">
-                <i className="pi pi-info-circle text-primary" onClick={(e) => op.current && op.current.toggle(e)}></i>
+                <i 
+                    className="pi pi-info-circle text-primary" 
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                ></i>
             </div>
             <p className="m-0 p-0 flex justify-content-center align-items-center absolute top-50 left-50 card-content text-base font-bold uppercase" style={{transform: 'translate(-50%, -50%)'}}>
                 {title}
             </p>
-            <OverlayPanel ref={op} className='w-4'>
+            <OverlayPanel ref={op} className="min-w-100 max-w-25rem break-word">
                 <p>{description}</p>
             </OverlayPanel>
         </div>
     )
 }
 
-export default WebsiteCard
+WebsiteCard.displayName = 'WebsiteCard';
+
+export default WebsiteCard;
