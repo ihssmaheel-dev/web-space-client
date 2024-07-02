7109e4f9b7b2325ce021490df7f3925c216659e1
ismail-hepl <ismail.s@hepl.com>
2024-07-02 22:40:49 +0530

    Feat: Add ThemeProvider in the main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './contexts/ThemeContexts.tsx';
 
 // Prime React Configuration
 import "primereact/resources/primereact.min.css";
-import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.min.css"; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
