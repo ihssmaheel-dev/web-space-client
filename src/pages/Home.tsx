36665d3cd3e656fa9efc9d5ff7a88d3a35f93c74
ismail-hepl <ismail.s@hepl.com>
2024-07-02 17:00:22 +0530

    Refactor: Optimize with type safety and optional chaining

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,13 +17,17 @@ interface WebsiteI {
 }
 
 const Home: React.FC = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState<number>(0);
 
     const categories: CategoryI[] = [
-        { name: 'Tab 1', icon: 'pi pi-fw pi-home', websites: [
-            { name: "google", description: "google", link: "www.google.com" },
-            { name: "youtube", description: "youtube", link: "www.youtube.com" }
-        ]},
+        {
+            name: 'Tab 1', 
+            icon: 'pi pi-fw pi-home', 
+            websites: [
+                { name: "google", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam voluptate magnam saepe enim! Nesciunt provident in dolore ipsa praesentium!", link: "www.google.com" },
+                { name: "youtube", description: "youtube", link: "www.youtube.com" }
+            ]
+        },
         { name: 'Tab 2', icon: 'pi pi-fw pi-calendar' },
         { name: 'Tab 3', icon: 'pi pi-fw pi-pencil' },
         { name: 'Tab 4', icon: 'pi pi-fw pi-file' },
@@ -33,15 +37,21 @@ const Home: React.FC = () => {
         { name: 'Tab 8', icon: 'pi pi-fw pi-heart' }
     ];
 
-    const tabHeaderTemplate = (category: CategoryI) => <span><i className={`${category.icon} tab-icon mr-2`}></i>{category.name}</span>;
+    const tabHeaderTemplate = (category: CategoryI) => (
+        <span>
+            <i className={`${category.icon} tab-icon mr-2`}></i>
+            {category.name}
+        </span>
+    );
+
     return (
         <div className="card py-3 px-6">
             <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} scrollable>
                 {categories.map((category, index) => (
-                    <TabPanel  key={index}  header={tabHeaderTemplate(category)} className="pt-3">
+                    <TabPanel key={index} header={tabHeaderTemplate(category)} className="pt-3">
                         <div className="grid">
-                            {category && category.websites && category?.websites.map((website, _index) => (
-                                <div className="col-2">
+                            {category.websites?.map((website, idx) => (
+                                <div key={idx} className="col-2">
                                     <WebsiteCard title={website.name} description={website.description} />
                                 </div>
                             ))}
