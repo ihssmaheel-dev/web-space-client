a7bc7168f92441678867c8cfc409a7e148577497
ismail-hepl <ismail.s@hepl.com>
2024-07-03 21:31:07 +0530

    Refactor: Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
