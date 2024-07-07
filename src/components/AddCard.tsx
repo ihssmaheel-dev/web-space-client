e70688994a00647b240d0ddeca769e39b7b36acc
ismail-hepl <ismail.s@hepl.com>
2024-07-07 13:47:09 +0530

    Feat: Add onClick props to handle the add website modal visibility

--- src/components/AddCard.tsx
+++ src/components/AddCard.tsx
@@ -1,13 +1,13 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Card } from 'primereact/card';
 
 interface AddCardProps {
-    
+    onClick: () => void;
 }
 
-const AddCard: React.FC<AddCardProps> = () => {
+const AddCard: React.FC<AddCardProps> = ({ onClick }) => {
     return (
-        <Card className="h-10rem cursor-pointer shadow-2 hover:shadow-8 transition-all transition-duration-300 flex align-items-center justify-content-center h-full">
+        <Card className="h-10rem cursor-pointer shadow-2 hover:shadow-8 transition-all transition-duration-300 flex align-items-center justify-content-center h-full" onClick={onClick}>
             <div className="border-circle" style={{ height: "60px", maxWidth: "60px" }}>
                 <i className={`pi pi-plus-circle`} style={{fontSize: "60px"}}></i>
             </div>
