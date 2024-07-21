81a21b49534aa9d616eb072c568b04d98658e001
ismail-hepl <ismail.s@hepl.com>
2024-07-21 23:06:39 +0530

    Feat: Create a index types file to handle the interface and types for better reusability

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type ImageType = "icon" | "image";
+
+export interface WebsiteI {
+    no: number;
+    id: string;
+    name: string;
+    image?: string;
+    imageType?: ImageType;
+    url: string;
+}
+
+export interface CategoryI {
+    no: number;
+    id: string;
+    name: string;
+    icon: string;
+    websites?: WebsiteI[];
+}
