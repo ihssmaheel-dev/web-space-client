d2a9894e8cad9d90f88799c3468e3b6ddc8f7f8e
ismail-hepl <ismail.s@hepl.com>
2024-08-07 18:31:48 +0530

    Feat: Create a setLocalStorageValue method in the localstorage utils

--- src/utils/localStorageUtils.ts
+++ src/utils/localStorageUtils.ts
@@ -7,3 +7,11 @@ export function getLocalStorageValue<T>(key: string): T | null {
         return null;
     }
 }
+
+export function setLocalStorageValue(key: string, value: any) {
+    try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+        console.warn('Error setting value in localStorage key “' + key + '”: ', error);
+    }
+}
