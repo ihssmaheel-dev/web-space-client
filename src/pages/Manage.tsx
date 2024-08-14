02980353bd5d133926c7ec8f026c0e5a90bbfe9e
ismail-hepl <ismail.s@hepl.com>
2024-08-14 21:18:55 +0530

    Feat: Implement the categories datatable in the manage page

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-import { CategoryI, WebsiteI } from '../types';
+import { CategoryI, SelectedWebsite, WebsiteI } from '../types';
 import { getLocalStorageValue } from '../utils/localStorageUtils';
 import EditCategoryModal from '../components/EditCategoryModal';
 import EditWebsiteModal from '../components/EditWebsiteModal';
@@ -18,18 +18,10 @@ import useTheme from '../hooks/useTheme';
 import useBookmarks from '../hooks/useBookmarks';
 import useCategories from '../hooks/useCategories';
 import useWebsites from '../hooks/useWebsites';
-
-interface SelectedWebsite {
-    categoryIndex: number;
-    websiteIndex: number;
-    title?: string;
-    website?: WebsiteI;
-}
+import CategoriesDatatable from '../components/CategoriesDatatable';
 
 const Manage: React.FC = () => {
-    const { theme } = useTheme();
     const { showToast } = useToast();
-    const { logVisit } = useUserActivity();
     const categoriesbbj = getLocalStorageValue<CategoryI[]>("categories") || [];
     const [categories, setCategories] = useLocalStorage("categories", categoriesbbj);
 
@@ -37,8 +29,6 @@ const Manage: React.FC = () => {
     const { handleAddCategory, handleUpdateCategory, handleDeleteCategory } = useCategories(categories, setCategories);
     const { handleAddWebsite, handleUpdateWebsite, handleDeleteWebsite } = useWebsites(categories, setCategories);
 
-    const userActivities: { [key: string]: number } = getLocalStorageValue("userActivity") || {};
-    const [expandedRows, setExpandedRows] = useState<any>(null);
     const [addCategoryVisible, setAddCategoryVisible] = useState(false);
     const [editCategoryVisible, setEditCategoryVisible] = useState(false);
     const [addWebsiteModalVisible, setAddWebsiteModalVisible] = useState(false);
@@ -47,26 +37,6 @@ const Manage: React.FC = () => {
     const [selectedCategoryIndex, setSelectedCategoryIndex] = useState<number>(0);
     const [confirmDeleteVisibleCategory, setConfirmDeleteVisibleCategory] = useState(false);
     const [confirmDeleteVisibleWebsite, setConfirmDeleteVisibleWebsite] = useState(false);
-
-    const handleEditCategory = (index: number) => {
-        setSelectedCategoryIndex(index);
-        setEditCategoryVisible(true);
-    };
-
-    const handleEditWebsite = (categoryIndex: number, websiteIndex: number) => {
-        setSelectedWebsite({ categoryIndex, websiteIndex, website: categories[categoryIndex]?.websites && categories[categoryIndex]?.websites[websiteIndex] });
-        setEditWebsiteModalVisible(true);
-    };
-
-    const handleDeleteCategoryClick = (index: number) => {
-        setSelectedCategoryIndex(index);
-        setConfirmDeleteVisibleCategory(true);
-    };
-
-    const handleWebsiteDeleteClick = (categoryIndex: number, websiteIndex: number) => {
-        setSelectedWebsite({ categoryIndex, websiteIndex, title: categories[categoryIndex].websites ? categories[categoryIndex].websites[websiteIndex].name : "" });
-        setConfirmDeleteVisibleWebsite(true);
-    };
 
     const confirmDeleteCategory = (e: React.MouseEvent) => {
         e.stopPropagation();
@@ -97,139 +67,6 @@ const Manage: React.FC = () => {
         setConfirmDeleteVisibleCategory(false);
     };
 
-    const handleOpenAll = (categoryIndex: number) => {
-        if (typeof categoryIndex === "number" && categories[categoryIndex]?.websites) {
-            const websites = categories[categoryIndex].websites;
-            websites?.forEach(website => {
-                logVisit(website.id);
-                window.open(website.url, "_blank");
-            });
-        } else {
-            console.error(`Invalid tab index or missing websites for tab index ${categoryIndex}`);
-        }
-    }
-
-    const handleCopyAll = (categoryIndex: number) => {
-        if (typeof categoryIndex === "number" && categories[categoryIndex]?.websites) {
-            const websites = categories[categoryIndex].websites;
-
-            let htmlTable = "<table style='border: 1px solid black; border-collapse: collapse;'>";
-            htmlTable += "<tr><th style='border: 1px solid black; padding: 5px;'>Name</th><th style='border: 1px solid black; padding: 5px;'>URL</th></tr>";
-
-            websites?.forEach(website => {
-                htmlTable += `<tr><td style='border: 1px solid black; padding: 5px;'>${website.name}</td><td style='border: 1px solid black; padding: 5px;'>${website.url}</td></tr>`;
-            });
-
-            htmlTable += "</table>";
-
-            const copyText = convert(htmlTable, {
-                wordwrap: 150,
-                tables: true,
-            });
-
-            navigator.clipboard.writeText(copyText)
-                .then(() => {
-                    showToast("success", "Copied to clipboard successfully");
-                })
-                .catch(err => {
-                    showToast("error", "Failed to copy to clipboard");
-                    console.error("Could not copy text: ", err);
-                });
-        }
-    };
-
-    const handleCopyUrl = (categoryIndex: number, websiteIndex: number) => {
-        const website = categories[categoryIndex]?.websites && categories[categoryIndex].websites[websiteIndex];
-
-        if (website) {
-            let copyText = website.url;
-            navigator.clipboard.writeText(copyText)
-                .then(() => {
-                    showToast("success", "Copied to clipboard successfully");
-                })
-                .catch(err => {
-                    showToast("error", "Failed to copy to clipboard");
-                    console.error("Could not copy text: ", err);
-                });
-        }
-    };
-
-    const customHeader = `custom-header ${theme === 'theme-light' && 'dark'}`;
-
-    const rowExpansionTemplate = (data: CategoryI) => {
-        return (
-            <>
-                <div className='flex align-items-center justify-content-end px-4'>
-                    <Button
-                        label="Add Website"
-                        icon="pi pi-plus"
-                        className="mt-2"
-                        onClick={() => {
-                            setSelectedCategoryIndex(data.no);
-                            setAddWebsiteModalVisible(true);
-                        }}
-                    />
-                </div>
-                <div className='px-4 py-4'>
-                    <DataTable value={data.websites} dataKey="id">
-                        <Column
-                            header="S.No."
-                            body={(_rowData: WebsiteI, options) => options.rowIndex + 1}
-                            headerClassName={customHeader}
-                        />
-                        <Column
-                            field="name"
-                            header="Website Name"
-                            headerClassName={customHeader}
-                            body={(rowData: WebsiteI) => (<a className='wrapping-cell' href={`${rowData?.url}`} target="_blank" onClick={() => logVisit(rowData?.id)} title={rowData?.name}>{rowData?.name}</a>) || ""}
-                        />
-                        <Column
-                            field="url"
-                            header="URL"
-                            headerClassName={customHeader}
-                            body={(rowData: WebsiteI) => (<a href={`${rowData?.url}`} className='url-cell' target="_blank" onClick={() => logVisit(rowData?.id)}>{rowData?.url}</a>)}
-                        />
-                        <Column
-                            field="icon"
-                            header="Icon / Image"
-                            headerClassName={customHeader}
-                            body={(rowData: WebsiteI) => (rowData.imageType === "icon" ? <i className={`pi ${rowData?.image} text-2xl`}></i> : <img src={rowData?.image} alt="icon" height={24} width={24} />) || ""}
-                        />
-                        <Column
-                            field="visits"
-                            header="Total Visits"
-                            headerClassName={customHeader}
-                            body={(rowData: WebsiteI) => userActivities[rowData?.id] || 0}
-                        />
-                        <Column
-                            header="Actions"
-                            headerClassName={customHeader}
-                            body={(_rowData: WebsiteI, options) => (
-                                <>
-                                    <Button
-                                        className='mr-2 custom-button'
-                                        icon="pi pi-copy font-semibold text-sm"
-                                        onClick={() => handleCopyUrl(data.no, options.rowIndex)}
-                                    />
-                                    <Button
-                                        className='bg-warning border-warning text-white mr-2 custom-button'
-                                        icon="pi pi-pencil font-semibold text-sm"
-                                        onClick={() => handleEditWebsite(data.no, options.rowIndex)}
-                                    />
-                                    <Button
-                                        className='bg-danger border-danger text-white custom-button'
-                                        icon="pi pi-trash text-sm"
-                                        onClick={() => handleWebsiteDeleteClick(data.no, options.rowIndex)}
-                                    />
-                                </>
-                            )}
-                        />
-                    </DataTable>
-                </div>
-            </>
-        );
-    };
-
     return (
         <div className='card py-4 px-4'>
             <div className='flex align-items-center justify-content-end'>
@@ -241,67 +78,7 @@ const Manage: React.FC = () => {
                     onClick={() => setAddCategoryVisible(true)}
                 />
             </div>
-            <DataTable
-                value={categories}
-                expandedRows={expandedRows}
-                onRowToggle={(e) => setExpandedRows(e.data)}
-                rowExpansionTemplate={rowExpansionTemplate}
-                dataKey="id"
-                className="custom-datatable"
-            >
-                <Column
-                    expander
-                    style={{ width: '6rem' }}
-                    headerClassName={customHeader}
-                />
-                <Column
-                    header="S.No."
-                    headerClassName={customHeader}
-                    body={(_rowData: CategoryI, options) => options.rowIndex + 1}
-                />
-                <Column
-                    field="name"
-                    header="Category Name"
-                    headerClassName={customHeader}
-                />
-                <Column
-                    header="No. of Websites"
-                    headerClassName={customHeader}
-                    body={(rowData: CategoryI) => rowData.websites?.length || 0}
-                />
-                <Column
-                    header="Actions"
-                    headerClassName={customHeader}
-                    body={(rowData: CategoryI, options) => (
-                        <>
-                            <Button
-                                className='mr-2 custom-button'
-                                icon="pi pi-external-link font-semibold text-sm"
-                                onClick={() => handleOpenAll(options.rowIndex)}
-                            />
-                            <Button
-                                className='mr-2 custom-button'
-                                icon="pi pi-copy font-semibold text-sm"
-                                onClick={() => handleCopyAll(options.rowIndex)}
-                            />
-                            {rowData.name.toLocaleLowerCase() !== ("Bookmarks Bar").toLocaleLowerCase() && (
-                                <>
-                                    <Button
-                                        className='bg-warning border-warning text-white mr-2 custom-button'
-                                        icon="pi pi-pencil font-semibold text-sm"
-                                        onClick={() => handleEditCategory(options.rowIndex)}
-                                    />
-                                    <Button
-                                        className='bg-danger border-danger text-white custom-button'
-                                        icon="pi pi-trash font-semibold text-sm"
-                                        onClick={() => handleDeleteCategoryClick(options.rowIndex)}
-                                    />
-                                </>
-                            )}
-                        </>
-                    )}
-                />
-            </DataTable>
+            <CategoriesDatatable setSelectedCategoryIndex={setSelectedCategoryIndex} setEditCategoryVisible={setEditCategoryVisible} setConfirmDeleteVisibleCategory={setConfirmDeleteVisibleCategory} setSelectedWebsite={setSelectedWebsite} setAddWebsiteModalVisible={setAddWebsiteModalVisible} setEditWebsiteModalVisible={setEditWebsiteModalVisible} setConfirmDeleteVisibleWebsite={setConfirmDeleteVisibleWebsite} />
 
             <AddCategoryModal
                 visible={addCategoryVisible}
